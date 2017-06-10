f7a796fe5bcd84f6002e99ea13eb22bf945fa2cd
Ian-Maynard <maynard.ian@gmail.com>
2017-06-10 11:56:25 -0400

    Multiple changes to schema and camp

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,1 +1,11 @@
 
+var express = require('express');
+var router = express.Router();
+var camp = require('../models/camp.js');
+
+// add a '/' endpoint that redirects to the /index route
+router.get('/', function(req, res) {
+	res.redirect('/index');
+});
+
+module.exports = router;

65f1a2585e2b08eaad05364924a7837fd46a42bd
Kristina Karrass <kristinakarrass@gmail.com>
2017-06-12 15:32:24 -0400

    working on markers for trails

--- public/assets/js/apicalls.js
+++ public/assets/js/apicalls.js
@@ -2,9 +2,16 @@ var lat = 0;
 var lng = 0;
 
 //define map
-L.mapbox.accessToken = 'pk.eyJ1Ijoia3Jpa2FyciIsImEiOiJjajEwcmxpdmEwM2ZoMzJwZWNrc3hnYm13In0.8cXei-iPLO0qctadLZ9O9w';
-var map = L.mapbox.map('map', 'mapbox.outdoors-v9')
-    .setView([15, -25], 2);
+mapboxgl.accessToken = 'pk.eyJ1Ijoia3Jpa2FyciIsImEiOiJjajEwcmxpdmEwM2ZoMzJwZWNrc3hnYm13In0.8cXei-iPLO0qctadLZ9O9w';
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/outdoors-v9', //stylesheet location
+    center: [-97.1191, 38.60313], //starting position
+    zoom: 2.9 //starting zoom
+});
+//add user's geolocation
+// map.addControl(new mapboxgl.GeolocateControl());
+
 //get user's location for search -- modify for 
 $(document).on("click", ".submit", function() {
     event.preventDefault();
@@ -31,6 +38,10 @@ $(document).on("click", ".submit", function() {
 //API call to get campgrounds in a 50 mile radius
 function campgroundCall() {
     $("#campgrounds").html("");
+    map.flyTo({
+        center: [lng, lat],
+        zoom: 9
+    });
     var queryURLfacility = "https://ridb.recreation.gov/api/v1/facilities/?activity=9&latitude=" + lat +
         "&longitude=" + lng + "&radius=50&apikey=1F46A83E349C407E8538DFA18D9C049A";
     console.log(queryURLfacility);
@@ -41,31 +52,35 @@ function campgroundCall() {
         console.log(response.RECDATA);
         var num = 1;
         for (var i = 0; i < response.RECDATA.length; i++) {
+            var campground = response.RECDATA[i];
             var res = $("<div class='info'>");
             // res.attr("data", response.RECDATA[i].FacilityID);
             var text = $("<p>");
             text.append(num + ". <br>");
-            text.append("Facility Name: " + response.RECDATA[i].FacilityName + "<br>");
+            text.append("Facility Name: " + campground.FacilityName + "<br>");
             // text.append("Facility ID: " + response.RECDATA[i].FacilityID + "<br>");
-            text.append("Description:   " + response.RECDATA[i].FacilityDescription + "<br>");
-            text.append("Directions:    " + response.RECDATA[i].FacilityDirections + "<br>");
+            text.append("Description:   " + campground.FacilityDescription + "<br>");
+            text.append("Directions:    " + campground.FacilityDirections + "<br>");
             //append body to div
             res.append(text);
             var search = $("<button class='campsite'>");
-            search.data("facID", response.RECDATA[i].FacilityID);
+            search.data("facID", campground.FacilityID);
             search.text("Choose Campsite");
             //append button to div to search campsites of facility
             res.append(search);
             res.append("<br><br><hr>");
             $("#campgrounds").append(res);
-            var marker = L.marker([lat, lng], {
-                    icon: L.mapbox.marker.icon({
-                        'marker-size': 'medium',
-                        'marker-color': '#4aaaa5'
-                    })
-
-                }) //popup showing user location and add it to map
-                .bindPopup(response.RECDATA[i].FacilityName)
+            // create the popup
+            var popup = new mapboxgl.Popup({ offset: 25 })
+                .setText('Campground Name: ' + campground.FacilityName);
+            // create DOM element for the marker
+            var el = document.createElement('div');
+            el.id = 'marker';
+            console.log(campground.FacilityLongitude, campground.FacilityLatitude);
+            // create the marker
+            new mapboxgl.Marker(el, { offset: [-25, -25] })
+                .setLngLat([campground.FacilityLongitude, campground.FacilityLatitude])
+                .setPopup(popup) // sets a popup on this marker
                 .addTo(map);
             num++;
         }
@@ -110,6 +125,10 @@ $(document).on("click", ".campsite", function() {
 //API call to get trails in a 50 mile radius around user's specified location
 function trailCall() {
     $("#trails").html("");
+    map.flyTo({
+        center: [lng, lat],
+        zoom: 9
+    });
     //ajax call to RIDB for USFS trails
     var queryURLtrails = "https://ridb.recreation.gov/api/v1/trails/USFS/?latitude=" + lat + "&longitude=" + lng + "&radius=100&apikey=1F46A83E349C407E8538DFA18D9C049A";
     console.log(queryURLtrails);
@@ -137,6 +156,18 @@ function trailCall() {
             res.append(search);
             res.append("<br><br><hr>");
             $("#trails").append(res);
+            var popup = new mapboxgl.Popup({ offset: 25 })
+                .setText('Trail Name: ' + response.RECDATA[i].TrailName +
+                        '<br>Trail Length: ' + response.RECDATA[i].SegmentLength);
+            // create DOM element for the marker
+            var el = document.createElement('div');
+            el.id = 'marker';
+            console.log(campground.FacilityLongitude, campground.FacilityLatitude);
+            // create the marker
+            new mapboxgl.Marker(el, { offset: [-25, -25] })
+                .setLngLat([campground.FacilityLongitude, campground.FacilityLatitude])
+                .setPopup(popup) // sets a popup on this marker
+                .addTo(map);
             num++;
         }
 

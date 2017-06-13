7e626daa0e4f7b63e076f9ca1e8e043b87901b4f
Zach Milosevich <zachmilo45@gmail.com>
2017-06-12 21:01:57 -0400

    merging my branch

---
+++ controllers/profileController.js
@@ -0,0 +1,11 @@
+const express = require("express");
+var Profile = require("../models/index").profile;
+var router = express.Router();
+
+
+
+
+
+
+
+module.exports = router;
--- routes/handbarsPageRoutes.js
+++ routes/handbarsPageRoutes.js
@@ -6,35 +6,31 @@ router.get("/", function (req, res) {
 });
 
 router.get("/backpack", function (req, res) {
-    res.render("index");
+    res.render("backpack");
 });
 
 router.get("/backpack/packer", function (req, res) {
-    res.render("index");
+    res.render("backPackPacker");
 });
 
 router.get("/campground", function (req, res) {
-    res.render("index");
+    res.render("campGroundResult");
 });
 
 router.get("/firstaid", function (req, res) {
-    res.render("index");
+    res.render("firstaid");
 });
 
 router.get("/mealplan", function (req, res) {
-    res.render("index");
+    res.render("mealPlan");
 });
 
 router.get("/profile", function (req, res) {
-    res.render("index");
+    res.render("ProfileMain");
 });
 
 router.get("/trail", function (req, res) {
-    res.render("index");
+    res.render("TrailResult");
 });
-
-
-
-
 
 module.exports = router;

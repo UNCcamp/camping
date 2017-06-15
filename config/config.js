a2516978f2450e89cf8902cdf151c717f786783b
Kristina Karrass <kristinakarrass@gmail.com>
2017-06-15 14:04:57 -0400

    trail and campground map images

---
+++ config/config.js
@@ -0,0 +1,4 @@
+var config = {
+	RIDBKey: "1F46A83E349C407E8538DFA18D9C049A",
+	mapboxKey: 'pk.eyJ1Ijoia3Jpa2FyciIsImEiOiJjajEwcmxpdmEwM2ZoMzJwZWNrc3hnYm13In0.8cXei-iPLO0qctadLZ9O9w'
+}
--- public/assets/js/apicalls.js
+++ public/assets/js/apicalls.js
@@ -64,7 +64,8 @@ function campgroundCall(lat, lng) {
             var campID = campground.FacilityID;
             var campLat = campground.FacilityLatitude;
             var campLng = campground.FacilityLongitude;
-            var imageURL = "https://api.mapbox.com/styles/v1/mapbox/outdoors-v9/static/geojson(%7B%22type%22%3A%22Point%22%2C%22coordinates%22%3A%5B" + campLng + "%2C" + campLat + "%5D%7D)/" + campLng + "," + campLat + ",12/250x250?access_token=" + mapboxKey;
+            var imageURL = "https://api.mapbox.com/styles/v1/mapbox/outdoors-v9/static/geojson(%7B%22type%22%3A%22Point%22%2C%22coordinates%22%3A%5B" 
+                           + campLng + "%2C" + campLat + "%5D%7D)/" + campLng + "," + campLat + ",12/250x250?access_token=" + mapboxKey;
             result.push({
                 name: campName,
                 description: campDesc,
@@ -170,7 +171,6 @@ function trailCall(lat, lng) {
             console.log(trailLat + ", " + trailLng);
             console.log(latlng);
             var imageURL = "https://api.mapbox.com/styles/v1/mapbox/outdoors-v9/static/geojson(%7B%22type%22%3A%22Point%22%2C%22coordinates%22%3A%5B" + trailLng + "%2C" + trailLat + "%5D%7D)/" + trailLng + "," + trailLat + ",12/250x250?access_token=" + mapboxKey;
-            console.log(imageURL);
             result.push({
                 name: trailName,
                 length: trailLength,

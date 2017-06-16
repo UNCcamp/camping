2bcdb96af1973380624203c717df3562e84a87f0
Matthew Johnson <mattj742@gmail.com>
2017-06-16 11:56:57 -0400

    updating tests

--- test/nightmare-test.js
+++ test/nightmare-test.js
@@ -14,41 +14,22 @@ nightmare
   .goto("https://unc-camp.herokuapp.com/")
   // Enter camp search.
   .type(".form-control", "Yosemite")
-<<<<<<< HEAD
   
   .click(".dropdown")
   .click("a[href='/backpack']")
   .wait("a[href='/backpack']")
-=======
-  // Enter trail search.
-  .type(".form-control", "Monument Valley")
-  // Click first aid link
-  .click("a[href='/firstaid']")
-  // Wait until the  link renders.
-  .wait("a[href='/firstaid']")
-  // Click a few items.
-  .click("#checkbox13")
-  .click("#checkbox15")
-  // Scroll down a few hundred pixels.
-  .scrollTo(500, 0)
-  .back()
-  .click(".dropdown")
-  .click("a[href='/backpack]")
-  .wait("a[href='/backpack]")
->>>>>>> dev
+
   .click("#checkbox1")
   .click("#checkbox5")
   .click("#checkbox6")
   .click("#checkbox10")
   .click("#checkbox13")
   .click(".btn-success")
-<<<<<<< HEAD
+
   // Scroll down a few hundred pixels.
   .scrollTo(500, 0)
   .back()
 
-=======
->>>>>>> dev
   // End test
   .end()
   // Execute commands

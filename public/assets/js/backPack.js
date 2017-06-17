377d55a7f92f9daa71e6c1108141ed4f7493ec60
Nate Pond <nathaniel.p.pond@gmail.com>
2017-06-16 20:49:46 -0400

    post added to js files for handlebars

--- public/assets/js/backPack.js
+++ public/assets/js/backPack.js
@@ -1,2 +1,52 @@
+var idnum = $("input:checkbox:not(:checked)").length + $("input:checked").length + 3;
+var prenum = idnum - 1
+var pre = "#checkbox" + prenum
+var id = "checkbox" + idnum
 
+
+$(".addItem").on("click", function(event) {
+  event.preventDefault();
+  var newItem = {
+    item: $("#NewItem").val().trim()
+  }
+
+  console.log(newItem)
+  $.post("/", newItem)
+    .done(function() {
+      var d = $("<div>");
+      d.addClass("funkyradio-success");
+      var i = $("<input>");
+      i.attr("type", "checkbox");
+      i.attr("name", "checkbox");
+      i.attr("id", id);
+      var l = $("<label>");
+      l.attr("for", id);
+      l.val("hello");
+      i.prepend(l);
+      d.prepend(i);
+      $("#item-area").prepend(d);
+    })
+
+});
+
+$.get("/", function(data) {
+
+  if (data.length !== 0) {
+
+    for (var i = 0; i < data.length; i++) {
+
+      var d = $("<div>");
+      d.addClass("funkyradio-success");
+
+      var i = $("<input>");
+      i.attr("type", "checkbox");
+      i.attr("name", "checkbox");
+      i.attr("id", this.id);
+      $("#item-area").prepend(d);
+
+    }
+
+  }
+
+});
 

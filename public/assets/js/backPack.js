f62056551b74970007f4589b303a852bc6f796e0
Nate Pond <nathaniel.p.pond@gmail.com>
2017-06-17 18:11:16 -0400

    backpack js

--- public/assets/js/backPack.js
+++ public/assets/js/backPack.js
@@ -1,32 +1,47 @@
+console.log("yo what the hell")
+
 var idnum = $("input:checkbox:not(:checked)").length + $("input:checked").length + 3;
 var prenum = idnum - 1
 var pre = "#checkbox" + prenum
 var id = "checkbox" + idnum
 
+var builtArray = []
 
-$(".addItem").on("click", function(event) {
-  event.preventDefault();
-  var newItem = {
-    item: $("#NewItem").val().trim()
-  }
 
-  console.log(newItem)
-  $.post("/", newItem)
-    .done(function() {
-      var d = $("<div>");
-      d.addClass("funkyradio-success");
-      var i = $("<input>");
-      i.attr("type", "checkbox");
-      i.attr("name", "checkbox");
-      i.attr("id", id);
-      var l = $("<label>");
-      l.attr("for", id);
-      l.val("hello");
-      i.prepend(l);
-      d.prepend(i);
-      $("#item-area").prepend(d);
+
+
+$("#buildBackPack").on("click", function(event) {
+  var s = $("input:checked").siblings();
+  console.log(s)
+  if (s.length > 0) {
+    $.each(s, function(index, value) {
+      var t = $(s[index]).text();
+      builtArray.push(t);
+      console.log(t)
     })
+    $.ajax({
+      method: "POST",
+      url: "",
+      data: { builtBackPack: builtArray }
+    }).done(function(msg) {
+      alert("Data saved:" + msg);
+    });
+  }
+})
 
+
+
+
+$("#addItem").on("click", function(event) {
+  var newItem = $("#newItem").val().trim()
+  $.ajax({
+      method: "POST",
+      url: "",
+      data: { addedItem: newItem }
+    }).done(function(msg) {
+      alert("Data saved:" + msg);
+    });
+  }
 });
 
 $.get("/", function(data) {
@@ -49,4 +64,3 @@ $.get("/", function(data) {
   }
 
 });
-

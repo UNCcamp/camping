4d88b0f5c865258893731630673a61fa089c46f1
Kristina Karrass <kristinakarrass@gmail.com>
2017-06-19 11:45:49 -0400

    map

--- public/assets/js/apicalls.js
+++ public/assets/js/apicalls.js
@@ -1,6 +1,6 @@
 //keys
-var mapboxKey = 'pk.eyJ1Ijoia3Jpa2FyciIsImEiOiJjajEwcmxpdmEwM2ZoMzJwZWNrc3hnYm13In0.8cXei-iPLO0qctadLZ9O9w';
-var RIDBkey = '1F46A83E349C407E8538DFA18D9C049A ';
+var mapboxKey ='pk.eyJ1Ijoia3Jpa2FyciIsImEiOiJjajEwcmxpdmEwM2ZoMzJwZWNrc3hnYm13In0.8cXei-iPLO0qctadLZ9O9w';
+var RIDBkey = '1F46A83E349C407E8538DFA18D9C049A';
 //handlebars reference
 var result;
 var lat = 0;
@@ -109,70 +109,6 @@ function campgroundCall(lat, lng, callback) {
     });
 }
 
-//API call to get campsites for specific campground
-// $(document).on("click", ".campsite", function() {
-//     console.log($(this).data("facID"));
-//     var facilityID = $(this).data("facID");
-//     var facilityLat = $(this).data("facLat");
-//     var facilityLong = $(this).data("facLong");
-//         map.flyTo({
-//         center: [facilityLong, facilityLat],
-//         zoom: 15
-//     });
-//     var queryURLcampsite = "https://ridb.recreation.gov/api/v1/facilities/" + facilityID + "/campsites/?apikey=1F46A83E349C407E8538DFA18D9C049A";
-//API call to get campsites for specific campground if we decide to go with this
-// function campsiteCall(campgroundID, campgroundLatitude, campgroundLongitude) {
-//
-//         map.flyTo({
-//         center: [campgroundLongitude, campgroundLatitude],
-//         zoom: 15
-//     });
-//     var queryURLcampsite = "https://ridb.recreation.gov/api/v1/facilities/" + campgroundID + "/campsites/?apikey=1F46A83E349C407E8538DFA18D9C049A";
-//     $.ajax({
-//         url: queryURLcampsite,
-//         method: 'GET'
-//     }).done(function(response) {
-//         console.log(response.RECDATA);
-//         var num = 1;
-//         for (var i = 0; i < response.RECDATA.length; i++) {
-//             var res = $("<div class='info'>");
-//             // res.attr("data", response.RECDATA[i].FacilityID);
-//             var text = $("<p>");
-//             text.append(num + ". <br>");
-//             text.append("Campsite Name: " + response.RECDATA[i].CampsiteName + "<br>");
-//             text.append("Campsite Type: " + response.RECDATA[i].CampsiteType + "<br>");
-//             text.append("Loop: " + response.RECDATA[i].Loop + "<br>");
-//             //append body to div
-//             res.append(text);
-//             var search = $("<button class='campsite'>");
-//             search.attr("data", response.RECDATA[i].CampsiteID);
-//             search.text("Choose Campsite");
-//             //append button to div to search campsites of facility
-//             res.append(search);
-//             res.append("<br><br><hr>");
-//             $("#campsites").append(res);
-//             num++;
-//         };
-//     });
-// });
-//         var result = [];
-//         for (var i = 0; i < response.RECDATA.length; i++) {
-//             var campsiteName = response.RECDATA[i].CampsiteName;
-//             var campsiteType = response.RECDATA[i].CampsiteType;
-//             var loop = response.RECDATA[i].Loop;
-//             results.push({
-//             name: campsiteName,
-//             type: campsiteType,
-//             loop: loop
-//             });
-//
-//             num++;
-//         }
-//         return result;
-//     });
-//   }
-
-
 //API call to get trails in a 50 mile radius around user's specified location
 function trailCall(lat, lng, callback) {
     map.flyTo({
@@ -209,7 +145,8 @@ function trailCall(lat, lng, callback) {
             var latlng = geoLine[0];
             var trailLng = geoLine[0][0];
             var trailLat = geoLine[0][1];
-            var imageURL = "https://api.mapbox.com/styles/v1/mapbox/outdoors-v9/static/geojson(%7B%22type%22%3A%22Point%22%2C%22coordinates%22%3A%5B" + trailLng + "%2C" + trailLat + "%5D%7D)/" + trailLng + "," + trailLat + ",12/250x250?access_token=" + mapboxKey;
+            var imageURL = "https://api.mapbox.com/styles/v1/mapbox/outdoors-v9/static/geojson(%7B%22type%22%3A%22Point%22%2C%22coordinates%22%3A%5B" + 
+                           trailLng + "%2C" + trailLat + "%5D%7D)/" + trailLng + "," + trailLat + ",12/250x250?access_token=" + mapboxKey;
             result.push({
                 name: trailName,
                 length: trailLength,
@@ -223,43 +160,3 @@ function trailCall(lat, lng, callback) {
         callback(result);
     });
 }
-
-// function to map trail GeoLine on map -- not working because it creates too many layers, work in progress
-
-// $(document).on("click", ".trail", function() {
-//     //get and modify trail's linestring
-//             var geoLine = $(this).data("LineString");
-//             console.log(geoLine);
-//             geoLine = geoLine.match(/\([^()]*\)/g);
-//             geoLine = geoLine[0].replace(/[\(\)]/g, '').split(', ');
-//             for (var j = 0; j < geoLine.length; j++) {
-//                 geoLine[j] = geoLine[j].split(" ");
-//             }
-//             for (var k = 0; k < geoLine.length; k++) {
-//                 geoLine[k][0] = parseFloat(geoLine[k][0]);
-//                 geoLine[k][1] = parseFloat(geoLine[k][1]);
-//             }
-//             console.log(geoLine);
-//             //  // add trail line to map
-//             map.addLayer({
-//                 "id": "route",
-//                 "type": "line",
-//                 "source": {
-//                     "type": "geojson",
-//                     "data": {
-//                         "type": "Feature",
-//                         "properties": {},
-//                         "geometry": {
-//                             "type": "LineString",
-//                             "coordinates": geoLine
-//                         }
-//                     }
-//                 },
-//                 "layout": {
-//                     "line-join": "round",
-//                     "line-cap": "round"
-//                 },
-//                 "paint": {
-//                     "line-color": "#888",
-//                     "line-width": 8
-//                 }
